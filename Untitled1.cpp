918b827c8a399a18f3c8f9bc4abb20a5ec6c39cf
thedude22 <amalik11@uic.edu>
2016-02-20 14:01:22 -0600

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+void instuctions(){
+	printf("Choose from the following options:\n 
+   		1. Encode some text\n                 
+   		2. Decode using user-entered values\n 
+   		3. Decode automatically\n             
+   		4. Exit program\n");	
+	
+}
 int main(void) {
 	// your code goes here
 	return 0;
